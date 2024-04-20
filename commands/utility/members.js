1bcca353b4b60f3e66d682aa19c4f34d37efaa2d
ibnaleem <ibnaleem@outlook.com>
2024-04-20 20:56:45 +0100

    use EmbedBuilder to customise members command's output

--- commands/utility/members.js
+++ commands/utility/members.js
@@ -1,10 +1,22 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('members')
-		.setDescription("Replies with this server's member count"),
-	async execute(interaction) {
-		await interaction.reply(`**${interaction.guild.memberCount}**`);
-	},
+    data: new SlashCommandBuilder()
+        .setName('members')
+        .setDescription("Replies with this server's member count"),
+    async execute(interaction) {
+        const guild = interaction.guild;
+        const commandInvoker = interaction.member;
+        const commandInvokerIcon = commandInvoker.avatarURL();
+        const memberCount = guild.memberCount;
+        const guildIcon = interaction.guild.iconURL();
+        const guildBanner = interaction.guild.bannerURL();
+
+        const membersEmbed = new EmbedBuilder()
+            .setColor(0x0c0c0d)
+            .setAuthor({ name: `${memberCount} members`, iconURL: guildIcon ? guildIcon : null })  
+			.setFooter({text: `requested by ${commandInvoker.displayName}`, iconURL: commandInvokerIcon ? commandInvokerIcon : null});
+        
+		await interaction.reply({ embeds: [membersEmbed] });
+    },
 };
